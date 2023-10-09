675805b7224ad8a2cd265faf05e1f43bd635fae7
chengjun.li <chengjun.li@sophgo.com>
2023-10-09 15:47:25 +0800

    Create msi-x whitelist and turn on msi-x for top intr

--- drivers/pci/controller/cadence/pcie-cadence-sophgo.h
+++ drivers/pci/controller/cadence/pcie-cadence-sophgo.h
@@ -1,6 +1,15 @@
 #ifndef PCIE_CADENCE_SOPHGO
 #define PCIE_CADENCE_SOPHGO
 
-extern struct irq_domain *cdns_pcie_get_parent_irq_domain(int intc_id);
 
+typedef struct {
+	const char  *name;
+	uint16_t    vendor_id;
+} vendor_id_list_t;
+
+extern vendor_id_list_t vendor_id_list[];
+extern size_t vendor_id_list_num;
+
+extern struct irq_domain *cdns_pcie_get_parent_irq_domain(int intc_id);
+int check_vendor_id(struct pci_dev *dev, vendor_id_list_t vendor_id_list[], size_t vendor_id_list_num);
 #endif

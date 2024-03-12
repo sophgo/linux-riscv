c-sky/csky-linux

driver: mmc: Add sophgo sg2042 soc support
Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>

drivers/mmc/host/sdhci.h
@@ -823,5 +823,8 @@ void sdhci_abort_tuning(struct sdhci_host *host, u32 opcode);
 void sdhci_switch_external_dma(struct sdhci_host *host, bool en);
 void sdhci_set_data_timeout_irq(struct sdhci_host *host, bool enable);
 void __sdhci_set_timeout(struct sdhci_host *host, struct mmc_command *cmd);
+#ifdef CONFIG_SOC_SOPHGO
+bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd);
+#endif
 
 #endif /* __SDHCI_HW_H */

sophgo/linux-riscv

drivers/mmc/host/sdhci.h
@@ -824,5 +824,8 @@ void sdhci_abort_tuning(struct sdhci_host *host, u32 opcode);
 void sdhci_switch_external_dma(struct sdhci_host *host, bool en);
 void sdhci_set_data_timeout_irq(struct sdhci_host *host, bool enable);
 void __sdhci_set_timeout(struct sdhci_host *host, struct mmc_command *cmd);
+#ifdef CONFIG_ARCH_SOPHGO
+bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd);
+#endif
 
 #endif /* __SDHCI_HW_H */
291de0d6865f996972131f3d234e64a7ad6b527e
zhaohui-yan <zhaohui.yan@sophgo.com>
2024-03-22 15:29:29 +0800

    Modify the switchtec's PCIE whitelist deviceId
    Signed-off-by: zhaohui-yan <zhaohui.yan@sophgo.com>

--- drivers/pci/controller/cadence/pcie-cadence-sophgo.c
+++ drivers/pci/controller/cadence/pcie-cadence-sophgo.c
@@ -463,7 +463,7 @@ static struct msi_domain_info cdns_pcie_top_intr_msi_domain_info = {
 struct vendor_id_list vendor_id_list[] = {
 	{"Inter X520", 0x8086, 0x10fb},
     //{"WangXun RP1000", 0x8088},
-	{"Switchtec", 0x11f8,4052},
+	{"Switchtec", 0x11f8,0x4052},
 };
 
 size_t vendor_id_list_num = ARRAY_SIZE(vendor_id_list);

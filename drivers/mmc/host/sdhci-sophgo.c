caiyuqing379/linux

riscv: add mmc driver acpi support

drivers/mmc/host/sdhci-sophgo.c
@@ -14,11 +14,13 @@
  *
  */
 
+#include <linux/acpi.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/delay.h>
 #include <linux/mmc/mmc.h>
 #include <linux/slab.h>
+#include <linux/pm_runtime.h>
 #include <linux/reset.h>
 #include "sdhci-pltfm.h"
 #include "sdhci-sophgo.h"
@@ -504,20 +506,29 @@ static const struct of_device_id sdhci_bm_dt_match[] = {
 
 MODULE_DEVICE_TABLE(of, sdhci_bm_dt_match);
 
+#ifdef CONFIG_ACPI
+static const struct acpi_device_id sdhci_bm_acpi_ids[] = {
+	{.id = "SGEM0000", .driver_data = (kernel_ulong_t)&sdhci_bm_emmc_pdata},
+	{.id = "SGSD0000", .driver_data = (kernel_ulong_t)&sdhci_bm_sd_pdata},
+	{}
+};
+MODULE_DEVICE_TABLE(acpi, sdhci_bm_acpi_ids);
+#endif
+
 static int sdhci_bm_probe(struct platform_device *pdev)
 {
+	struct device *dev = &pdev->dev;
 	struct sdhci_host *host;
 	struct sdhci_pltfm_host *pltfm_host;
 	struct sdhci_bm_host *bm_host;
-	const struct of_device_id *match;
 	const struct sdhci_pltfm_data *pdata;
 	int ret;
 
-	match = of_match_device(sdhci_bm_dt_match, &pdev->dev);
-	if (!match)
-		return -EINVAL;
-
-	pdata = match->data;
+	pdata = device_get_match_data(&pdev->dev);
+	if (!pdata) {
+		dev_err(&pdev->dev, "Error: No device match data found\n");
+		return -ENODEV;
+	}
 
 	host = sdhci_pltfm_init(pdev, pdata, sizeof(*bm_host));
 	if (IS_ERR(host))
@@ -535,50 +546,52 @@ static int sdhci_bm_probe(struct platform_device *pdev)
 
 	sdhci_get_of_property(pdev);
 
-	if (host->mmc->caps2 & MMC_CAP2_NO_SD) {
-		bm_host->reset = devm_reset_control_get(&pdev->dev, "emmc");
-		if (IS_ERR(bm_host->reset)) {
-			ret = PTR_ERR(bm_host->reset);
-			goto pltfm_free;
-		}
+  if (dev->of_node) {
+	  if (host->mmc->caps2 & MMC_CAP2_NO_SD) {
+		  bm_host->reset = devm_reset_control_get(&pdev->dev, "emmc");
+		  if (IS_ERR(bm_host->reset)) {
+			  ret = PTR_ERR(bm_host->reset);
+			  goto pltfm_free;
+		  }
 
-		bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_emmc");
-		if (IS_ERR(bm_host->clkaxi))
-			dev_err(&pdev->dev, "get emmc clk axi failed\n");
-		else
-			clk_prepare_enable(bm_host->clkaxi);
-		bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_emmc");
-		if (IS_ERR(bm_host->clk))
-			dev_err(&pdev->dev, "get emmc clk failed\n");
-		else
-			clk_prepare_enable(bm_host->clk);
-		bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_emmc");
-		if (IS_ERR(bm_host->clk100k))
-			dev_err(&pdev->dev, "get emmc clk 100k failed\n");
-		else
-			clk_prepare_enable(bm_host->clk100k);
-	} else if (host->mmc->caps2 & MMC_CAP2_NO_MMC) {
-		bm_host->reset = devm_reset_control_get(&pdev->dev, "sdio");
-		if (IS_ERR(bm_host->reset)) {
-			ret = PTR_ERR(bm_host->reset);
-			goto pltfm_free;
-		}
+		  bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_emmc");
+		  if (IS_ERR(bm_host->clkaxi))
+			  dev_err(&pdev->dev, "get emmc clk axi failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clkaxi);
+		  bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_emmc");
+		  if (IS_ERR(bm_host->clk))
+			  dev_err(&pdev->dev, "get emmc clk failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk);
+		  bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_emmc");
+		  if (IS_ERR(bm_host->clk100k))
+			  dev_err(&pdev->dev, "get emmc clk 100k failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk100k);
+	  } else if (host->mmc->caps2 & MMC_CAP2_NO_MMC) {
+		  bm_host->reset = devm_reset_control_get(&pdev->dev, "sdio");
+		  if (IS_ERR(bm_host->reset)) {
+			  ret = PTR_ERR(bm_host->reset);
+			  goto pltfm_free;
+		  }
 
-		bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_sd");
-		if (IS_ERR(bm_host->clkaxi))
-			dev_err(&pdev->dev, "get sd clk axi failed\n");
-		else
-			clk_prepare_enable(bm_host->clkaxi);
-		bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_sd");
-		if (IS_ERR(bm_host->clk))
-			dev_err(&pdev->dev, "get sd clk failed\n");
-		else
-			clk_prepare_enable(bm_host->clk);
-		bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_sd");
-		if (IS_ERR(bm_host->clk100k))
-			dev_err(&pdev->dev, "get sd clk 100k failed\n");
-		else
-			clk_prepare_enable(bm_host->clk100k);
+		  bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_sd");
+		  if (IS_ERR(bm_host->clkaxi))
+			  dev_err(&pdev->dev, "get sd clk axi failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clkaxi);
+		  bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_sd");
+		  if (IS_ERR(bm_host->clk))
+			  dev_err(&pdev->dev, "get sd clk failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk);
+		  bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_sd");
+		  if (IS_ERR(bm_host->clk100k))
+			  dev_err(&pdev->dev, "get sd clk 100k failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk100k);
+	  }
 	}
 
 	host->mmc_host_ops.select_drive_strength = sdhci_bm_select_drive_strength;
@@ -611,6 +624,7 @@ static struct platform_driver sdhci_bm_driver = {
 	.driver = {
 		.name = DRIVER_NAME,
 		.of_match_table = sdhci_bm_dt_match,
+		.acpi_match_table = ACPI_PTR(sdhci_bm_acpi_ids),
 	},
 };
 

sophgo/linux-riscv

Signed-off-by: caiyuqing379 <caiyuqing_hz@outlook.com>

drivers/mmc/host/sdhci-sophgo.c
@@ -14,11 +14,13 @@
  *
  */
 
+#include <linux/acpi.h>
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/delay.h>
 #include <linux/mmc/mmc.h>
 #include <linux/slab.h>
+#include <linux/pm_runtime.h>
 #include <linux/reset.h>
 #include "sdhci-pltfm.h"
 #include "sdhci-sophgo.h"
@@ -504,20 +506,29 @@ static const struct of_device_id sdhci_bm_dt_match[] = {
 
 MODULE_DEVICE_TABLE(of, sdhci_bm_dt_match);
 
+#ifdef CONFIG_ACPI
+static const struct acpi_device_id sdhci_bm_acpi_ids[] = {
+	{.id = "SGPH0015", .driver_data = (kernel_ulong_t)&sdhci_bm_emmc_pdata},
+	{.id = "SGPH0016", .driver_data = (kernel_ulong_t)&sdhci_bm_sd_pdata},
+	{}
+};
+MODULE_DEVICE_TABLE(acpi, sdhci_bm_acpi_ids);
+#endif
+
 static int sdhci_bm_probe(struct platform_device *pdev)
 {
+	struct device *dev = &pdev->dev;
 	struct sdhci_host *host;
 	struct sdhci_pltfm_host *pltfm_host;
 	struct sdhci_bm_host *bm_host;
-	const struct of_device_id *match;
 	const struct sdhci_pltfm_data *pdata;
 	int ret;
 
-	match = of_match_device(sdhci_bm_dt_match, &pdev->dev);
-	if (!match)
-		return -EINVAL;
-
-	pdata = match->data;
+	pdata = device_get_match_data(&pdev->dev);
+	if (!pdata) {
+		dev_err(&pdev->dev, "Error: No device match data found\n");
+		return -ENODEV;
+	}
 
 	host = sdhci_pltfm_init(pdev, pdata, sizeof(*bm_host));
 	if (IS_ERR(host))
@@ -535,50 +546,52 @@ static int sdhci_bm_probe(struct platform_device *pdev)
 
 	sdhci_get_of_property(pdev);
 
-	if (host->mmc->caps2 & MMC_CAP2_NO_SD) {
-		bm_host->reset = devm_reset_control_get(&pdev->dev, "emmc");
-		if (IS_ERR(bm_host->reset)) {
-			ret = PTR_ERR(bm_host->reset);
-			goto pltfm_free;
-		}
+  if (dev->of_node) {
+	  if (host->mmc->caps2 & MMC_CAP2_NO_SD) {
+		  bm_host->reset = devm_reset_control_get(&pdev->dev, "emmc");
+		  if (IS_ERR(bm_host->reset)) {
+			  ret = PTR_ERR(bm_host->reset);
+			  goto pltfm_free;
+		  }
 
-		bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_emmc");
-		if (IS_ERR(bm_host->clkaxi))
-			dev_err(&pdev->dev, "get emmc clk axi failed\n");
-		else
-			clk_prepare_enable(bm_host->clkaxi);
-		bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_emmc");
-		if (IS_ERR(bm_host->clk))
-			dev_err(&pdev->dev, "get emmc clk failed\n");
-		else
-			clk_prepare_enable(bm_host->clk);
-		bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_emmc");
-		if (IS_ERR(bm_host->clk100k))
-			dev_err(&pdev->dev, "get emmc clk 100k failed\n");
-		else
-			clk_prepare_enable(bm_host->clk100k);
-	} else if (host->mmc->caps2 & MMC_CAP2_NO_MMC) {
-		bm_host->reset = devm_reset_control_get(&pdev->dev, "sdio");
-		if (IS_ERR(bm_host->reset)) {
-			ret = PTR_ERR(bm_host->reset);
-			goto pltfm_free;
-		}
+		  bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_emmc");
+		  if (IS_ERR(bm_host->clkaxi))
+			  dev_err(&pdev->dev, "get emmc clk axi failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clkaxi);
+		  bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_emmc");
+		  if (IS_ERR(bm_host->clk))
+			  dev_err(&pdev->dev, "get emmc clk failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk);
+		  bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_emmc");
+		  if (IS_ERR(bm_host->clk100k))
+			  dev_err(&pdev->dev, "get emmc clk 100k failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk100k);
+	  } else if (host->mmc->caps2 & MMC_CAP2_NO_MMC) {
+		  bm_host->reset = devm_reset_control_get(&pdev->dev, "sdio");
+		  if (IS_ERR(bm_host->reset)) {
+			  ret = PTR_ERR(bm_host->reset);
+			  goto pltfm_free;
+		  }
 
-		bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_sd");
-		if (IS_ERR(bm_host->clkaxi))
-			dev_err(&pdev->dev, "get sd clk axi failed\n");
-		else
-			clk_prepare_enable(bm_host->clkaxi);
-		bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_sd");
-		if (IS_ERR(bm_host->clk))
-			dev_err(&pdev->dev, "get sd clk failed\n");
-		else
-			clk_prepare_enable(bm_host->clk);
-		bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_sd");
-		if (IS_ERR(bm_host->clk100k))
-			dev_err(&pdev->dev, "get sd clk 100k failed\n");
-		else
-			clk_prepare_enable(bm_host->clk100k);
+		  bm_host->clkaxi = devm_clk_get(&pdev->dev, "clk_gate_axi_sd");
+		  if (IS_ERR(bm_host->clkaxi))
+			  dev_err(&pdev->dev, "get sd clk axi failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clkaxi);
+		  bm_host->clk = devm_clk_get(&pdev->dev, "clk_gate_sd");
+		  if (IS_ERR(bm_host->clk))
+			  dev_err(&pdev->dev, "get sd clk failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk);
+		  bm_host->clk100k = devm_clk_get(&pdev->dev, "clk_gate_100k_sd");
+		  if (IS_ERR(bm_host->clk100k))
+			  dev_err(&pdev->dev, "get sd clk 100k failed\n");
+		  else
+			  clk_prepare_enable(bm_host->clk100k);
+	  }
 	}
 
 	host->mmc_host_ops.select_drive_strength = sdhci_bm_select_drive_strength;
@@ -611,6 +624,7 @@ static struct platform_driver sdhci_bm_driver = {
 	.driver = {
 		.name = DRIVER_NAME,
 		.of_match_table = sdhci_bm_dt_match,
+		.acpi_match_table = ACPI_PTR(sdhci_bm_acpi_ids),
 	},
 };
 
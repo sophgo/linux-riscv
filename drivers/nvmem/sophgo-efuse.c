cf9322069d40f01c31cb098dbe62e68d3dadde44
zhencheng.zhang <zhencheng.zhang@sophgo.com>
2025-01-03 11:50:16 +0800

    nvmem: Support two efuse controllers
    - delete double bit operation, so cell is hard macro address
    - add efuse1 node in dts
    
    Signed-off-by: zhencheng.zhang <zhencheng.zhang@sophgo.com>

--- drivers/nvmem/sophgo-efuse.c
+++ drivers/nvmem/sophgo-efuse.c
@@ -11,17 +11,11 @@
 #include <linux/property.h>
 #include <linux/of.h>
 #include <linux/clk.h>
+#include <linux/bitops.h>
 
-#define cpu_write32(a, v)	writel(v, a)
-#define cpu_read32(a)		readl(a)
-
-#define NUM_ADDRESS_BITS	(efuse->num_addr_bits)
-#define EFUSE_BASE		(efuse->regs)
-#define EFUSE_MODE		(efuse->regs)
-#define EFUSE_ADR		(efuse->regs + 0x4)
-#define EFUSE_RD_DATA		(efuse->regs + 0xc)
-
-static struct sg_efuse_device *efuse;
+#define EFUSE_MODE		0
+#define EFUSE_ADR		0x4
+#define EFUSE_RD_DATA		0xc
 
 struct sg_efuse_device {
 	struct device *dev;
@@ -38,85 +32,93 @@ struct sg_efuse_device {
 	struct mutex efuse_mutex;
 };
 
-static void efuse_mode_wait_ready(void)
+static inline void efuse_write32(struct sg_efuse_device *efuse, uint32_t offset, uint32_t v)
 {
-	while ((cpu_read32(EFUSE_MODE) & 0b11) != 0)
+	writel(v, efuse->regs + offset);
+}
+
+static inline uint32_t efuse_read32(struct sg_efuse_device *efuse, uint32_t offset)
+{
+	return readl(efuse->regs + offset);
+}
+
+static void efuse_mode_wait_ready(struct sg_efuse_device *efuse)
+{
+	while ((efuse_read32(efuse, EFUSE_MODE) & 0b11) != 0)
 		;
 }
 
-static void efuse_mode_reset(void)
+static void efuse_mode_reset(struct sg_efuse_device *efuse)
 {
-	cpu_write32(EFUSE_MODE, 0);
-	efuse_mode_wait_ready();
+	efuse_write32(efuse, EFUSE_MODE, 0);
+	efuse_mode_wait_ready(efuse);
 }
 
-static void efuse_mode_md_write(u32 val)
+static void efuse_mode_md_write(struct sg_efuse_device *efuse, u32 val)
 {
-	u32 mode = cpu_read32(EFUSE_MODE);
+	u32 mode = efuse_read32(efuse, EFUSE_MODE);
 	u32 new = (mode & 0xfffffffc) | (val & 0b11);
 
-	cpu_write32(EFUSE_MODE, new);
+	efuse_write32(efuse, EFUSE_MODE, new);
 }
 
-static u32 make_adr_val(u32 address, u32 bit_i)
+static u32 make_adr_val(struct sg_efuse_device *efuse, u32 address, u32 bit_i)
 {
-	const u32 address_mask = (1 << NUM_ADDRESS_BITS) - 1;
+	const u32 address_mask = (1 << efuse->num_addr_bits) - 1;
 
 	return (address & address_mask) |
-		((bit_i & 0x1f) << NUM_ADDRESS_BITS);
+		((bit_i & 0x1f) << efuse->num_addr_bits);
 }
 
-static void efuse_set_bit(uint32_t address, uint32_t bit_i)
+static void efuse_set_bit(struct sg_efuse_device *efuse, uint32_t address, uint32_t bit_i)
 {
-	u32 adr_val = make_adr_val(address, bit_i);
+	u32 adr_val = make_adr_val(efuse, address, bit_i);
 
-	cpu_write32(EFUSE_ADR, adr_val);
-	efuse_mode_md_write(0b11);
-	efuse_mode_wait_ready();
+	efuse_write32(efuse, EFUSE_ADR, adr_val);
+	efuse_mode_md_write(efuse, 0b11);
+	efuse_mode_wait_ready(efuse);
 }
 
-static u32 efuse_embedded_read(u32 address)
+/*used to read hard macro address*/
+static u32 efuse_embedded_read(struct sg_efuse_device *efuse, u32 address)
 {
 	u32 adr_val;
 	u32 ret =  0;
 
-	address *= 2; /*double bit*/
-
 	mutex_lock(&efuse->efuse_mutex);
 
-	efuse_mode_reset();
-	adr_val = make_adr_val(address, 0);
-	cpu_write32(EFUSE_ADR, adr_val);
-	efuse_mode_md_write(0b10);
-	efuse_mode_wait_ready();
-	ret = cpu_read32(EFUSE_RD_DATA);
+	efuse_mode_reset(efuse);
+	adr_val = make_adr_val(efuse, address, 0);
+	efuse_write32(efuse, EFUSE_ADR, adr_val);
+	efuse_mode_md_write(efuse, 0b10);
+	efuse_mode_wait_ready(efuse);
+	ret = efuse_read32(efuse, EFUSE_RD_DATA);
 
 	mutex_unlock(&efuse->efuse_mutex);
 
 	return ret;
 }
 
-static void efuse_embedded_write(u32 address, u32 val)
+/*used to write hard macro address*/
+static void efuse_embedded_write(struct sg_efuse_device *efuse, u32 address, u32 val)
 {
-	address *= 2; /*double bit*/
-
 	mutex_lock(&efuse->efuse_mutex);
 
 	for (int i = 0; i < 32; i++)
 		if ((val >> i) & 1)
-			efuse_set_bit(address, i);
+			efuse_set_bit(efuse, address, i);
 
 	mutex_unlock(&efuse->efuse_mutex);
 }
 
-static u32 nvmem_read_cell(int offset)
+static u32 nvmem_read_cell(struct sg_efuse_device *efuse, int offset)
 {
-	return efuse_embedded_read(offset);
+	return efuse_embedded_read(efuse, offset);
 }
 
-static void nvmem_write_cell(int offset, u32 value)
+static void nvmem_write_cell(struct sg_efuse_device *efuse, int offset, u32 value)
 {
-	efuse_embedded_write(offset, value);
+	efuse_embedded_write(efuse, offset, value);
 }
 
 static int sophgo_nvmem_read(void *context, unsigned int off, void *val, size_t count)
@@ -124,6 +126,7 @@ static int sophgo_nvmem_read(void *context, unsigned int off, void *val, size_t 
 	int size, left, start, i;
 	u32 tmp;
 	u8 *dst;
+	struct sg_efuse_device *efuse = context;
 
 	left = count;
 	dst = val;
@@ -131,7 +134,7 @@ static int sophgo_nvmem_read(void *context, unsigned int off, void *val, size_t 
 	if (off & 0x03) {
 		size = min_t(int, 4 - (off & 0x03), left);
 		start = (off & 0x03);
-		tmp = nvmem_read_cell(off >> 2);
+		tmp = nvmem_read_cell(efuse, off >> 2);
 		memcpy(dst, &((u8 *)&tmp)[start], size);
 		dst += size;
 		left -= size;
@@ -141,7 +144,7 @@ static int sophgo_nvmem_read(void *context, unsigned int off, void *val, size_t 
 	/* body */
 	size = left >> 2;
 	for (i = 0; i < size; ++i) {
-		tmp = nvmem_read_cell(off >> 2);
+		tmp = nvmem_read_cell(efuse, off >> 2);
 		memcpy(dst, &tmp, 4);
 		dst += 4;
 		left -= 4;
@@ -150,7 +153,7 @@ static int sophgo_nvmem_read(void *context, unsigned int off, void *val, size_t 
 
 	/* tail */
 	if (left) {
-		tmp = nvmem_read_cell(off >> 2);
+		tmp = nvmem_read_cell(efuse, off >> 2);
 		memcpy(dst, &tmp, left);
 	}
 
@@ -163,6 +166,7 @@ static int sophgo_nvmem_write(void *context, unsigned int off, void *val, size_t
 	int size, left, start, i;
 	u32 tmp;
 	u8 *dst;
+	struct sg_efuse_device *efuse = context;
 
 	left = count;
 	dst = val;
@@ -172,7 +176,7 @@ static int sophgo_nvmem_write(void *context, unsigned int off, void *val, size_t
 		size = min_t(int, 4 - (off & 0x03), left);
 		start = (off & 0x03);
 		memcpy(&((u8 *)&tmp)[start], dst, size);
-		nvmem_write_cell(off >> 2, tmp);
+		nvmem_write_cell(efuse, off >> 2, tmp);
 		dst += size;
 		left -= size;
 		off += size;
@@ -182,7 +186,7 @@ static int sophgo_nvmem_write(void *context, unsigned int off, void *val, size_t
 	size = left >> 2;
 	for (i = 0; i < size; ++i) {
 		memcpy(&tmp, dst, 4);
-		nvmem_write_cell(off >> 2, tmp);
+		nvmem_write_cell(efuse, off >> 2, tmp);
 		dst += 4;
 		left -= 4;
 		off += 4;
@@ -192,7 +196,7 @@ static int sophgo_nvmem_write(void *context, unsigned int off, void *val, size_t
 	if (left) {
 		tmp = 0;
 		memcpy(&tmp, dst, left);
-		nvmem_write_cell(off >> 2, tmp);
+		nvmem_write_cell(efuse, off >> 2, tmp);
 	}
 
 	return 0;
@@ -204,6 +208,7 @@ static int sg_efuse_probe(struct platform_device *pdev)
 	int ret;
 	struct nvmem_config sg_efuse_cfg;
 	struct nvmem_device *nvmem;
+	struct sg_efuse_device *efuse;
 
 	efuse = devm_kzalloc(&pdev->dev, sizeof(*efuse), GFP_KERNEL);
 
@@ -212,9 +217,9 @@ static int sg_efuse_probe(struct platform_device *pdev)
 	if (IS_ERR(efuse->regs))
 		return PTR_ERR(efuse->regs);
 
-	device_property_read_u32(&pdev->dev, "num_address_bits", &efuse->num_addr_bits);
 	device_property_read_u32(&pdev->dev, "num_cells", &efuse->num_cells);
-	device_property_read_u32(&pdev->dev, "cell_width", &efuse->cell_width);
+	efuse->num_addr_bits = ilog2(efuse->num_cells);
+
 	mutex_init(&efuse->efuse_mutex);
 
 	base_clk = devm_clk_get(&pdev->dev, "base_clk");
@@ -235,12 +240,13 @@ static int sg_efuse_probe(struct platform_device *pdev)
 			dev_err(&pdev->dev, "failed to enable apb clock\n");
 	}
 
-	sg_efuse_cfg.name = "sophgo_efuse",
+	sg_efuse_cfg.name = "sophgo_efuse";
+	sg_efuse_cfg.id = NVMEM_DEVID_AUTO;
 	sg_efuse_cfg.read_only = false,
 	sg_efuse_cfg.root_only = true,
 	sg_efuse_cfg.reg_read = sophgo_nvmem_read,
 	sg_efuse_cfg.reg_write = sophgo_nvmem_write,
-	sg_efuse_cfg.size = efuse->num_cells * efuse->cell_width,
+	sg_efuse_cfg.size = efuse->num_cells * 4,
 	/* efuse is 32bits per cell, but symmetric keys in real word usually
 	 * byte sequence, so i implements a least significant byte first scheme.
 	 * for symmetric key stored msb in efuse.
@@ -261,12 +267,6 @@ static int sg_efuse_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static void sg_efuse_remove(struct platform_device *pdev)
-{
-	nvmem_unregister(efuse->nvdev);
-	return;
-}
-
 static const struct of_device_id sg_efuse_dt_ids[] = {
 	{
 		.compatible = "sg,efuse",
@@ -278,7 +278,6 @@ MODULE_DEVICE_TABLE(of, sg_efuse_dt_ids);
 
 static struct platform_driver sg_efuse_platform_driver = {
 	.probe = sg_efuse_probe,
-	.remove = sg_efuse_remove,
 	.driver = {
 		.name = "sg,efuse",
 		.of_match_table = sg_efuse_dt_ids,
